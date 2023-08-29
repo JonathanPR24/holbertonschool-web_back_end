5fb11531113b08f30a80c17f055f29ecc13995c2
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-29 17:18:22 -0400

    Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains variables and functions with annotated values."""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
