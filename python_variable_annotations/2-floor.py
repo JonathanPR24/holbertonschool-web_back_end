ad9d1635aa8a5fa5379a9ed582e4834239d35bd6
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-29 16:07:10 -0400

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate the floor of a float.
+"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+   """ Calculates the floor of a float."""
+    return math.floor(n)
