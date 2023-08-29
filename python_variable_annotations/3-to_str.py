0717d052b66463341aeff3745e5d8807c7027142
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-29 16:19:33 -0400

    Basic annotations - to string

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -2,10 +2,5 @@
 """This script defines a function to_str that converts a float to its string representation."""
 
 def to_str(n: float) -> str:
-    """
-    Convert a float to its string representation.
-
-    Returns:
-        str: The string representation of the input float.
-    """
+"""Convert a float to its string representation."""
     return str(n)
