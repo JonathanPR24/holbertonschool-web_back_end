7d57de07205e5de879c84314bbb7a1dd05237d37
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-30 17:28:27 -0400

    Importing wait_random from 0-basic_async_syntax

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,22 +2,14 @@
 """
 Script containing the task_wait_random function.
 """
-
 import asyncio
-from typing import Coroutine
-
+from typing import any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Coroutine:
     """
     Returns an asyncio.Task that runs the wait_random coroutine.
-
-    Args:
-        max_delay (int): The maximum delay for wait_random.
-
-    Returns:
-        asyncio.Task: A task to run the wait_random coroutine.
     """
     task = asyncio.create_task(wait_random(max_delay))
     return task
