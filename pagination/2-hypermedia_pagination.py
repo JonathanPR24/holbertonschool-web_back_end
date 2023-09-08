3e97258a8eb2540b4687196b5f28ee9e318155a6
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-08 14:55:21 -0400

    Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -4,11 +4,13 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
+import math
 from typing import List, Dict
 
-class Server:
-    """Server class to paginate a database of popular baby names."""
 
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -16,7 +18,8 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Retrieve and cache the dataset, excluding the header."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -26,24 +29,18 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Create and cache an indexed version of the dataset."""
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Retrieve a page of data based on the given index and page size.
-
-        Args:
-            index (int, optional): The starting index (default is None).
-            page_size (int, optional): The number of items per page (default is 10).
-
-        Returns:
-            Dict: A dictionary containing page information, including data and next index.
-        """
+        """method that get the hyper index"""
 
         result_dataset = []
         index_data = self.indexed_dataset()
