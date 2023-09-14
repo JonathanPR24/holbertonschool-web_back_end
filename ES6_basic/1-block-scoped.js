98a278989c8ad6659e12e0627c4435e224a283df
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-14 16:09:26 -0400

    Block Scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false; // Use let instead of var
+  let task2 = true;  // Use let instead of var
+
+  if (trueOrFalse) {
+    task = true; // Assign values directly without re-declaring
+    task2 = false; // Assign values directly without re-declaring
+  }
+
+  return [task, task2];
+}
