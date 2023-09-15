1a1ca3dc071556f7f27ead3c46463f10686c5feb
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-15 18:07:48 -0400

    Implement a class named Airport

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,7 +1,11 @@
-// 7-main.js
-
-import Airport from './7-airport.js';
-
-const airportSF = new Airport('San Francisco Airport', 'SFO');
-console.log(airportSF);
-console.log(airportSF.toString());
+/* eslint-disable */
+export default class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    toString() {
+      return `[${this._code}] ${this._name}`;
+    }
+}
