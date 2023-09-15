20af9bc66b49f533a0d6df1cfe9dedbe9adba46d
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-15 13:24:03 -0400

    Import the ClassRoom class from 0-classroom.js

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+// 1-make_classrooms.js
+
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  // Create an array of ClassRoom objects with the specified sizes
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+
+  return rooms;
+}
