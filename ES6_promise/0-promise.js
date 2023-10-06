f5ec4eeda0cc2ef0c901e29783973772e36c8f19
JonathanPR24 <jonathanperez604@gmail.com>
2023-10-06 19:23:17 -0400

    Return a Promise using this prototype function getResponseFromAPI()

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,4 @@
 /* promise need a function to work */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation (e.g., an API call)
-    setTimeout(() => {
-      // Simulate a successful response
-      resolve("API response data");
-
-      // Simulate an error
-      // reject(new Error("API request failed"));
-    }, 1000); // Simulate a delay of 1 second
-  });
+  return new Promise((resolve, reject) => [resolve, reject]);
 }
