10dea78b32298503daab692fbaedf9acb113825a
JonathanPR24 <jonathanperez604@gmail.com>
2023-10-10 15:29:52 -0400

    Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].body.firstName} ${values[1].body.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
