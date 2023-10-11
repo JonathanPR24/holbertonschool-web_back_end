503d445327f29df2a26a04ad6d30e03ebecaca7a
JonathanPR24 <jonathanperez604@gmail.com>
2023-10-10 20:54:09 -0400

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,15 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promises).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => {
+      for (const x of res) {
+        if (x.status === 'rejected') {
+          x.value = `Error: ${x.reason.message}`;
+          delete x.reason;
+        }
+      }
+      return res;
+    });
 }
